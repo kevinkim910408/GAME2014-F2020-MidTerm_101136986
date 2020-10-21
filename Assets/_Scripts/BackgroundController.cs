17f90ab2067dae14b969774aaa04c2e0a9c60097
kevinkim910408 <kevinkim0408@naver.com>
2020-10-21 14:17:13 +0900

    The Starred Background assets scroll in from the right to left
    The Starred Background assets will scroll in from the right-side of the screen to the left-side of the screen instead of from the top to the bottom.

--- Assets/_Scripts/BackgroundController.cs
+++ Assets/_Scripts/BackgroundController.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///Student name : Junho Kim
+///Student ID : 101136986
+///Source file name : BackgroundController.cs
+///Date last modified : October, 21. 2020
+///Program description : To controll the backgrounds (ex: scrolling)
+///Revision history : October, 21. 2020 : Added the speed and boundary for horizontal mode, changed the functions' variables to horizontal mode
+/// </summary>
+
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    //public float verticalSpeed;         // y - axis
+    //public float verticalBoundary;
+    public float horizontalSpeed;          // x - axis
+    public float horizontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -16,18 +27,18 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(horizontalBoundary, 0.0f);
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -horizontalBoundary)
         {
             _Reset();
         }
