b944f1894bdb95c363debba515d5e858f7da98b7
kevinkim910408 <kevinkim0408@naver.com>
2020-10-21 14:38:32 +0900

    Touch Input, Player Movement and rotated to face the right-side, also cannot leave the borders,
    c. The Player’s Ship starts on the Left-hand side of the screen and in a fixed X-position (similar to having a fixed Y-position at the bottom of the screen). (5 points)
    d. The Player’s Ship will move with Touch Input. (5 points)
    e. The Player's Ship will be rotated to face the right-side of the screen. (5 points)
    g. The Player’s Ship’s motion will be limited so that it cannot leave the borders of the screen. (5 points)

--- Assets/_Scripts/BackgroundController.cs
+++ Assets/_Scripts/BackgroundController.cs
@@ -8,16 +8,24 @@ using UnityEngine;
 ///Source file name : BackgroundController.cs
 ///Date last modified : October, 21. 2020
 ///Program description : To controll the backgrounds (ex: scrolling)
-///Revision history : October, 21. 2020 : Added the speed and boundary for horizontal mode, changed the functions' variables to horizontal mode
+///Revision history : October, 21. 2020 : Added the speed and boundary for horizontal mode, changed the functions' variables to horizontal mode, Inline comments
 /// </summary>
 
 public class BackgroundController : MonoBehaviour
 {
+    #region Variables
     //public float verticalSpeed;         // y - axis
     //public float verticalBoundary;
+
+    [Header("Scrolling Speed")]
     public float horizontalSpeed;          // x - axis
+
+    [Header("Boundary Check")]
     public float horizontalBoundary;
 
+    #endregion
+
+    #region Unity_Method
     // Update is called once per frame
     void Update()
     {
@@ -25,16 +33,23 @@ public class BackgroundController : MonoBehaviour
         _CheckBounds();
     }
 
+    #endregion
+
+    #region Custom_Method
+
+    // to reset background position
     private void _Reset()
     {
         transform.position = new Vector3(horizontalBoundary, 0.0f);
     }
 
+    // background move horizontally. ( right side to left side)
     private void _Move()
     {
         transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
+    // to check bounds. If backgrounds hit this bounds, reset their position.
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
@@ -43,4 +58,5 @@ public class BackgroundController : MonoBehaviour
             _Reset();
         }
     }
+    #endregion
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,17 +4,32 @@ using System.ComponentModel;
 using UnityEditor;
 using UnityEngine;
 
+/// <summary>
+///Student name : Junho Kim
+///Student ID : 101136986
+///Source file name : BackgroundController.cs
+///Date last modified : October, 21. 2020
+///Program description : To controll the Player (ex: movement with inputs, checking boundary, )
+///Revision history : October, 21. 2020 : Touch Input change to vertical mode, Modify player speed and boundary to vertical mode, inline comments
+/// </summary>
+
 public class PlayerController : MonoBehaviour
 {
+    #region
+    // to use bullet manager
     public BulletManager bulletManager;
 
     [Header("Boundary Check")]
-    public float horizontalBoundary;
+    //public float horizontalBoundary;
+    public float verticalBoundary;
 
     [Header("Player Speed")]
-    public float horizontalSpeed;
+   // public float horizontalSpeed;
+    public float verticalSpeed;
     public float maxSpeed;
-    public float horizontalTValue;
+
+    //public float horizontalTValue;
+    public float verticalTValue;
 
     [Header("Bullet Firing")]
     public float fireDelay;
@@ -23,6 +38,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D m_rigidBody;
     private Vector3 m_touchesEnded;
 
+
+    #endregion
+
+    #region Unity_Method
     // Start is called before the first frame update
     void Start()
     {
@@ -38,7 +57,12 @@ public class PlayerController : MonoBehaviour
         _FireBullet();
     }
 
-     private void _FireBullet()
+    #endregion
+
+    #region Custom_Method
+
+    // for firing bullets by frame count
+    private void _FireBullet()
     {
         // delay bullet firing 
         if(Time.frameCount % 60 == 0 && bulletManager.HasBullets())
@@ -47,6 +71,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // player movement with touch screen and keyboard
     private void _Move()
     {
         float direction = 0.0f;
@@ -56,13 +81,13 @@ public class PlayerController : MonoBehaviour
         {
             var worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
 
-            if (worldTouch.x > transform.position.x)
+            if (worldTouch.x > transform.position.y)
             {
                 // direction is positive
                 direction = 1.0f;
             }
 
-            if (worldTouch.x < transform.position.x)
+            if (worldTouch.x < transform.position.y)
             {
                 // direction is negative
                 direction = -1.0f;
@@ -73,43 +98,48 @@ public class PlayerController : MonoBehaviour
         }
 
         // keyboard support
-        if (Input.GetAxis("Horizontal") >= 0.1f) 
+        if (Input.GetAxis("Vertical") >= 0.1f) 
         {
             // direction is positive
             direction = 1.0f;
         }
 
-        if (Input.GetAxis("Horizontal") <= -0.1f)
+        if (Input.GetAxis("Vertical") <= -0.1f)
         {
             // direction is negative
             direction = -1.0f;
         }
 
-        if (m_touchesEnded.x != 0.0f)
+        if (m_touchesEnded.y != 0.0f)
         {
-           transform.position = new Vector2(Mathf.Lerp(transform.position.x, m_touchesEnded.x, horizontalTValue), transform.position.y);
+            transform.position = new Vector2(transform.position.x, Mathf.Lerp(transform.position.y, m_touchesEnded.y, verticalTValue));
         }
         else
         {
-            Vector2 newVelocity = m_rigidBody.velocity + new Vector2(direction * horizontalSpeed, 0.0f);
+            Vector2 newVelocity = m_rigidBody.velocity + new Vector2(0.0f, direction * verticalSpeed);
             m_rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
             m_rigidBody.velocity *= 0.99f;
         }
     }
 
+    // player boundary check, so player cannot go out of the screen
     private void _CheckBounds()
     {
         // check right bounds
-        if (transform.position.x >= horizontalBoundary)
+        if (transform.position.y >= verticalBoundary)
         {
-            transform.position = new Vector3(horizontalBoundary, transform.position.y, 0.0f);
+            //transform.position = new Vector3(verticalBoundary, transform.position.y, 0.0f);
+            transform.position = new Vector3(transform.position.x, verticalBoundary, 0.0f);
         }
 
         // check left bounds
-        if (transform.position.x <= -horizontalBoundary)
+        if (transform.position.y <= -verticalBoundary)
         {
-            transform.position = new Vector3(-horizontalBoundary, transform.position.y, 0.0f);
+            //transform.position = new Vector3(-verticalBoundary, transform.position.y, 0.0f);
+            transform.position = new Vector3(transform.position.x, -verticalBoundary, 0.0f);
         }
 
     }
+
+    #endregion
 }
